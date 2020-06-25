a9e62c368722ede0975e9cdc70273927b41eea1a
abolfazlmim <afmmyam@gmail.com>
2020-06-25 22:49:07 +0400

    Create Hello-world.go

---
+++ Hello-world.go
@@ -0,0 +1,13 @@
+package Golang
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello world")
+
+}
+
+//Run With :go run Hello-world.go
+// Build Exe File: go build hello-world.go
+//ls
+//./hello-world
